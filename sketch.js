a276665334e8be1b69b2ca7dde1bee0ca4c99587
mistermasternimish <81861117+mistermasternimish@users.noreply.github.com>
2021-05-09 14:31:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+var grid = 50;
+var width = 1366;
+var carGroup1,logGroup1;
+var grassHeight = 100;
+var gameState = "play";
+var carAnimation, logAnimation, playerAnimation;
+var school;
+function preload()
+{
+ 
+}
+
+function setup() {
+  createCanvas(1366,2700);
+  carGroup1 = new Group();
+  logGroup1 = new Group();
+  
+  for(vari=0;1<;i++){
+var bottobGrass1= createsprite(683,hight-50-(i=400),width,grassHeight);
+of(i=2%===0)
+{
+var (road=createsprite(683,hight-50-(i=400)-grasshight,width,300.)
+road.shapecolour="black"
+ bottomgrass.shapecolour="grey"
+
+for(vari=0;i < 40;i++
+cars= new car(2);
+cargroup1.add(cars.spt)
+
+ {
+
+
+
+
+
+
+
+}
+
+
+   
+ }
+
+function draw() {
+  background("skyblue");
+ 
+ 
+
+  drawSprites();
+}
+
